330528bc2ada142183e5d591457726735305d26c
chinyi <ncy77862@gmail.com>
2026-07-15 15:28:34 +0800

    Feat: undate pages templates

--- config/settings.py
+++ config/settings.py
@@ -123,6 +123,10 @@ USE_I18N = True
 USE_TZ = True
 
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_URL ="static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'config/static')]
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
